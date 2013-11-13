bb363d88f098f50df6fdde309daaa776f62d63ab
drunia <pass1032>
2013-11-13 22:29:12 +0200

    sleep

--- DRSSRegConfirm.js
+++ DRSSRegConfirm.js
@@ -4,24 +4,43 @@
  */
  
 /**
- * Registering events on DRSSRegConfirm.html
+ * Registering start app point on DRSSRegConfirm.html
  */
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('#start').addEventListener('click', start);
-});
+document.addEventListener('DOMContentLoaded', main);
  
 /**
- * Create notification to notificate user when work is done
+ * Main start application point
  */
-
-
+function main() {
+	document.querySelector('#addButton').addEventListener('click', addToZoTable);
+} 
 
 /**
- * Start working with DRSS
+ * Add new people into printing table
  */
-function start() {
-	var notification = webkitNotifications.createNotification(
-		"icon.png", "Отчет о проделаной работе", "Привет Сеня!!!"
-	);
-	notification.show();
+function addToZoTable() {
+	var idCode = document.querySelector('#IdInput').value;
+	var fio = document.querySelector('#FIOInput').value;
+	
+	if (trim(idCode == "") || trim(fio == "")) {
+		alert("Пустые значения недопустимы!");
+		return;
+	}
+}
+ 
+/**
+ * Create notification to notificate user 
+ */
+function notificate(title, message) {
+	webkitNotifications.createNotification("icon.png", title, message).show();
+}
+
+/**
+ * Delete spaces from string
+ */
+function trim(str) { 
+	str += " ";
+	str = str.replace(/^\ /, '');
+	str = str.replace(/\ $/, '');
+	return str;
 }

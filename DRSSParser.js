018a85dea86d8eb2174b8cf5dea8ad17e6a9fbd9
drunia <druniax@gmail.com>
2013-11-20 16:55:18 +0200

    save

--- DRSSParser.js
+++ DRSSParser.js
@@ -6,6 +6,7 @@
 var WORK_STATUS = { PRINTING: 1, READY: 2, ERROR: 3 };
 var MESSAGES    = { CLOSE_TAB: 10 };
 var currInQueue = null;
+var registered  = null;
 
 /**
  * Parser start her
@@ -43,8 +44,19 @@ var currInQueue = null;
 					break;		
 					//p_flow_step_id = 1 - Find page
 					case "1":
-						console.log("DRSS parser: Detect DRSS Find page, try query by people...");
+						console.log("DRSS parser: Location DRSS Find page, try query by people...");
 						var regInfo = DRSSQueryDataByPeople(storage);
+						if (regInfo != null) print(regInfo, storage);
+					break;
+					//p_flow_step_id = 128 - Advanced registration info page
+					case "128":
+						console.log("DRSS parser: Location DRSS advanced registration info page, parse...");
+						DRSSEnterToMDZU();
+					break;
+					//p_flow_step_id = 128 - Advanced registration info page [Дані МДЗУ]
+					case "138":
+						console.log("DRSS parser: Location DRSS advanced registration info page [Дані МДЗУ], parse...");
+						var regInfo = DRSSParseRegData();
 						if (regInfo != null) print(regInfo, storage);
 					break;
 					default: 
@@ -81,13 +93,14 @@ function DRSSLogin(storage) {
  * Open [Страхувальники (МДЗУ)] link from MainMenu DRSS
  */
 function DRSSOpenFindPage(storage) {
-	var DRSSFindMenuItemValue = "Страхувальники (МДЗУ)";
+	var DRSSFindMenuItemValue = "Страхувальники";
 	var DRSSFindMenuItems = document.querySelectorAll(".dhtmlSubMenuN");
 	var DRSSFindMenuItem = null;
 	try {
 		for (var i = 0; i < DRSSFindMenuItems.length; i++) {
 			if (DRSSFindMenuItems[i].innerHTML.trim() == DRSSFindMenuItemValue)
 				DRSSFindMenuItem = DRSSFindMenuItems[i];
+				break;
 			}
 			if (DRSSFindMenuItem != null) {
 				console.log("DRSS parser: MainmenuItem [" + DRSSFindMenuItemValue + "] finded, entering...");
@@ -118,10 +131,16 @@ function DRSSQueryDataByPeople(storage) {
 			var findButtonValue = "Пошук";
 			var findButton = null;
 			for (var i = 0; i < findButtons.length; i++) {
-				if (findButtons[i].innerHTML.trim() == findButtonValue) 
+				if (findButtons[i].innerHTML.trim() == findButtonValue) {
 					findButton = findButtons[i];
+					break;
+				}
 			}
 			if (findButton != null) {
+				var selRegionFilter = document.querySelector("#P1_OPFU_REG");
+				var selRaionFilter = document.querySelector("#P1_OPFU_DISTR");
+				selRegionFilter.selectedIndex = 0; //select "всі" 
+				selRaionFilter.selectedIndex = 0; //select "всі"
 				idInput.value = currInQueue.split(",")[0];
 				findButton.click();
 				console.log("DRSS parser: Query registration data...");
@@ -139,18 +158,37 @@ function DRSSQueryDataByPeople(storage) {
 				console.log("DRSS parser: Registration data found OK, try parse...");
 				var statusImages = document.querySelectorAll("img");
 				var statusRegValue = "Зареєстровано";
+				var statusUnRegValue = "Знято з обліку";
 				var regDataLink = null;
+				var unRegDataLink = null;
 				for (var i = 0; i < statusImages.length; i++) {
+					//find registered status "Зареєстровано"
 					if (statusImages[i].title.trim() == statusRegValue) {
 						//link to advanced registration data
 						regDataLink = statusImages[i].parentNode;
+						break;
+					}
+					//find unregistered status "Знято з обліку"
+					if (statusImages[i].title.trim() == statusUnRegValue) {
+						//link to advanced registration data
+						unRegDataLink = statusImages[i].parentNode;
 					}
 				}
-				if (regDataLink != null) {
-					//way out from this method...
+				if (regDataLink != null || unRegDataLink != null) {
+					//if data exist in statuses "Зареєстровано" or "Знято з обліку"
 					console.log("DRSS parser: Entering to advanced registration data...");
-					regDataLink.click();
-					return null;
+					if (regDataLink != null) {
+						console.log("DRSS parser: Entering to REGISTER status...");
+						regDataLink.click();
+						registered = true;
+						return null;
+					}
+					if (unRegDataLink != null) {
+						console.log("DRSS parser: Entering to UNREGISTER status...");
+						unRegDataLink.click();
+						registered = false;
+						return null;
+					}		
 				} else {
 					//data found, but status "registered" not found
 					console.log("DRSS parser: Status \"" + statusRegValue + "\" not found!");
@@ -176,6 +214,54 @@ function DRSSQueryDataByPeople(storage) {
 }
 
 /**
+ * Entering to MDZU tab
+ */
+function DRSSEnterToMDZU() {
+	var links = document.querySelector("a");
+	var MDZUDataTitle = "Дані МДЗУ";
+	var MDZUDataLink = null;
+	try {
+		for (var i = 0; i < links.length; i++) {
+			if (links[i].title.trim() == MDZUDataTitle) {
+				MDZUDataLink = links[i];
+				break;
+			}
+		}
+		if (MDZUDataLink != null) {
+			MDZUDataLink.click();
+		} else {
+			alert("Ошибочка вышла!\DRSSEnterToMDZU(): link to tab [MDZU] not finded");
+			return false;
+		}
+	} catch (e) {
+		alert("Ошибочка вышла!\DRSSEnterToMDZU(): " + e);
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Parsing registration data on page
+ * Returned regInfo object
+ */
+function DRSSParseRegData() {
+	var regInfo = { id: null, fio: null, register: null, regFromTo: null };
+	var regFromInput = document.querySelector("#P138_IM_DT_PSV");
+	var regToInput = document.querySelector("P138_IAB_STOP_DT");
+	if (regFromInput == null) {
+		alert("Ошибочка вышла!\DRSSParseRegData(): regFrom data not found!");
+		return null;
+	}
+	if (regToInput == null)
+		console.log("DRSS parser: DRSSParseRegData(): regTo data not found!");
+	regInfo.id  = currInQueue.split(",")[0];
+	regInfo.fio = currInQueue.split(",")[1];
+	regInfo.register = registered;
+	regInfo.regFromTo = regFromInput.value + ";" + regToInput.value;
+	return regInfo;
+}
+
+/**
  * Print in existig tab
  */
 function print(regInfo, storage) {
@@ -197,8 +283,8 @@ function print(regInfo, storage) {
 	//foot
 	document.write(
 		"<table width='100%'><tr><td align='left'><b>Начальник відділу<br>персоніфікованого обліку<br>інформаційних систем та мереж</b></td>" + 
-		"<td align='right' valign='bottom'><b>/__________ / &nbsp <p style='text-decoration:underline; display: inline'>/" + storage.boss + "/<br></p></b></td></tr>" +
-		"<tr><td align='left' valign='bottom'>Виконавець:</td><td align='right'>М.П.<br>/__________ / &nbsp " +
+		"<td align='right' valign='bottom'><b>__________  &nbsp <p style='text-decoration:underline; display: inline'>/" + storage.boss + "/<br></p></b></td></tr>" +
+		"<tr><td align='left' valign='bottom'>Виконавець:</td><td align='right'>М.П.<br>__________  &nbsp " +
 		"<p style='text-decoration:underline; display: inline'>/" + storage.operator + "/</p></td></tr></table>"
 	);
 	document.close();
